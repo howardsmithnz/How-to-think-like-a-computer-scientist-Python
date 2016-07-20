ed1ef9675dfaf15ea75bc954e00b0f50f196e09b
Howard Smith <howardsmithnz@gmail.com>
2016-07-21 10:34:40 +1200

    first commit

---
+++ Ch1.py
@@ -0,0 +1,9 @@
+
+# How to think like a computer scientist - learn with Python
+
+# Ch.1
+
+# 1. this sentence is not correct   This correct is sentence not.
+
+# 2. The / operator performs integer division. This is not what most people 
+#    expect. e.g. 3/4 => 0 (not 0.75)
