dd270c3eb7442bcc0252022ef86fadfbe5920618
Howard Smith <howardsmithnz@gmail.com>
2016-07-21 19:02:11 +1200

    create Ch2.py

---
+++ Ch2.py
@@ -0,0 +1,23 @@
+ 
+ # How to think like a computer scientist - learn with Python
+
+# Ch.2
+
+# 1. - a syntax error, prints 12, prints values.
+#     - a print statement may be followed by an expression
+#     - it returns a printable representation of an expression
+
+# 2. 
+w1 = "All"
+w2 = " work"
+w3 = " and"
+w4 = " no"
+w5 = " play"
+w6 = " makes"
+w7 = " Jack"
+w8 = " a"
+w9 = " dull"
+w10 = " boy."
+
+print w1 + w2 + w3 + w4 + w5 + w6 + w7 + w8 + w9 + w10
+ 
