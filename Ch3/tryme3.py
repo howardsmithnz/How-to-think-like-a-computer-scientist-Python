8d23388033d92f892acfde206e8df036ec7b89eb
Howard Smith <howardsmithnz@gmail.com>
2016-07-27 12:24:29 +1200

    add Ch3 Ex1

---
+++ Ch3/tryme3.py
@@ -0,0 +1,24 @@
+# How to think like a computer scientist - learn with Python
+
+# Ch.3
+# tryme3.py
+
+def new_line():
+  print
+
+def three_lines():
+  new_line()
+  new_line()
+  new_line()
+
+def nine_lines():
+  three_lines()
+  three_lines()
+  three_lines()
+
+def clear_screen():
+  for i in range(25):
+    print
+    i += 1
+
+clear_screen()
